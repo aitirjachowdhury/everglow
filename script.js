e13ce64e2ed8894089074bf3341ccd71361f522e
Aiti <48098217+aitirjachowdhury@users.noreply.github.com>
2022-11-29 18:00:20 -0500

    second publish

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-let slideIndex = 1;
-show(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  show(slideIndex += n);
-}
-
-function show(n) {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  let demo = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  //does not display the other pictures
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-  captionText.innerHTML = demo[slideIndex - 1].alt; //shows the image captions
-}
-
-//Registering for consultation
-var consult = document.querySelector("#consult")
-consult.addEventListener("click", function(){
-  alert("Please scroll down to the Contact Us section and fill in your details. We will get back to you in 1-2 business days.")
-  location.href = "faq.html"
-})
-
-//Takes user to social media account.
-var follow = document.querySelector("#flw")
-follow.addEventListener("click", function(){
-  alert("We are working on our social media page. Come back again! We appreciate your patience during this time.")
-})
-
-
 function myFunction() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
@@ -53,28 +12,33 @@ function myFunction() {
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
-    } else {
+    }
+    else {
       li[i].style.display = "none";
     }
   }
 }
 
-//calculating skin type//
-document.getElementById("form1").onsubmit=function() {
-       variable = parseInt(document.querySelector('input[name = "one"]:checked').value);
-	   sub = parseInt(document.querySelector('input[name = "two"]:checked').value);
-	   con = parseInt(document.querySelector('input[name = "three"]:checked').value);
-	   ifstate = parseInt(document.querySelector('input[name = "four"]:checked').value);
+//Registering for consultation
+var consultBut = document.querySelector("#consult");
+consultBut.addEventListener("click", bookConsultation);
+function bookConsultation() {
+  alert("Please scroll down to the Contact Us section and fill in your details. We will get back to you in 1-2 business days.");
+  location.href = "faq.html";
+}
 
+//Takes user to social media account.
+var followBut = document.querySelector("#flw");
+followBut.addEventListener("click", followPage);
+function followPage() {
+  alert("We are working on our social media page. Come back again! We appreciate your patience during this time.");
+}
 
-	   result = one + two + three + four;
+//contact us submit button
 
-	document.getElementById("grade").innerHTML = result;
-
-
-
-return false;
-
+function contactConfirm() {
+  alert("Thank you for submitting your information. We will be in touch with you shortly!");
+}
 
 /*
 var acc = document.getElementsByClassName("accordion");
@@ -91,3 +55,50 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 */
+
+/* FAQ */
+
+// var q1 = document.querySelector("#q1");
+// q1.addEventListener("mouseover", mOver);
+
+// function mouseOver(){
+//   alert();
+//   // question.innerHTML = "Visit the Skin Type page to do a skin test, and this test is free.";
+// }
+
+function q1Over() {
+  document.querySelector("#a1").textContent = "Yes. The test is free! Visit the Skin Type page to do a skin test, and this test is completely free"
+}
+function q1Out() {
+  document.querySelector("#a1").textContent = ""
+}
+function q2Over() {
+  document.querySelector("#a2").textContent = "No. This website only stores recommendation of products that may be suitable for you. You cannot purchase anything through this website. However, we do provide links to the site that do offer the products for you to purchase."
+}
+function q2Out() {
+  document.querySelector("#a2").textContent = ""
+}
+function q3Over() {
+  document.querySelector("#a3").textContent = "Simply visit our home page and click on Book a Consultation Today button!"
+}
+function q3Out() {
+  document.querySelector("#a3").textContent = ""
+}
+function q4Over() {
+  document.querySelector("#a4").textContent = "Please scroll down on this page, and fill out the Contact Us form. We will be in touch shortly!"
+}
+function q4Out() {
+  document.querySelector("#a4").textContent = ""
+}
+
+function q5Over() {
+  document.querySelector("#a5").textContent = "For suggestions on any of the content on our page, please contact us using the form below."
+}
+
+function q5Out() {
+  document.querySelector("#a5").textContent = ""
+}
+
+function contactConfirm(){
+  alert("Thank you for submiting! We will be in touch with you soon!")
+}
